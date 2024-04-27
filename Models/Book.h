a658fa6b09d8a3e04bacb0d9935bca1286c04a8e
Mirza <b5504682@mail.ru>
2024-04-27 16:14:56 +0400

    Book.h and User.h structures were edited and supplemented.

--- Models/Book.h
+++ Models/Book.h
@@ -3,6 +3,10 @@ typedef struct
     int id;
     char title[50];
     char author[50];
-    char genre[50];
-    int quantity;
+    char genre[25];
+    float price;
+    int quantityForRent;
+    int quantityForSale; 
+    int rentalDuration;
+    unsigned int popularity; //from 0 to 100
 } Book;
